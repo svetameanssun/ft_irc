35ccf208c6d463f52ccdb693e4e07c63385baee3
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-09-10 13:49:18 +0200

    Update ParcerUtils.cpp
    In development

--- ParcerUtils.cpp
+++ ParcerUtils.cpp
@@ -1,10 +1,53 @@
 #include "Parcer.hpp"
 
+/*template <typename T>
+return_type fucntion_name(T parameter){
+}*/
+template <typename T>
+int checkSenderCommand(
+
+void Parcer::checkUserCommand(void){
+
+}
+/**
+* @brief Sets the prefix type for the message sender type (SERV or USER)
+*
+* NOW this function assigns the default prefix type to the given reference.
+* LATER this funtion will allow distinguishing whether the sender
+* is a server or a user, through accesing the Server/Client class.
+*
+* @param prefixType Reference to a prefix type variable that will be set.
+*	By default, it will be set to PREF_USER.
+*/
 void Parcer::setPrefixType(pT &prefixType){
-	// in the future this way I will know, 
-	// who is sending the message - a server or a user
-	// by default it will be PREF_USER
+	/*code to develop...*/
 	prefixType = PREF_USER;
+}
+
+/**
+* @brief Checks if the command is valid.
+*
+* Checks if the word written for the command is valid,
+* AND that the command written corresponds to the type of the
+* sender (server/user).
+* To do so, parcer class has a variable that sets
+* the structure pT(prefixType) to the needed value,
+* so this fucntion needs to access only its own class value _prefixType.
+*
+* @param message Reference to a string message that is beeing sent
+*			
+*/
+void Parcer::checkCommand(std::string &message){
+	if (_prefixType == PREF_USER)
+	{
+		//check if the command corresponds to the list of the commands
+		// of the client.
+	}
+	else if (_prefixType == PREF_SERVER)
+	{
+		// check if the command corresponds to the list of the commands
+		// of the server.
+	}
 }
 
 
@@ -24,14 +67,12 @@ std::vector <std::string> Parcer::customSplit(){
 	catch (const std::exception & e){
 		std::cerr << "input error: " << e.what();
 	}
-	setPrefixType(_prefixType);
-	if (_prefixType == PREF_USER)
-	{
-		checkUserCommand();
+	setPrefixType(_prefixType);// I set the value to default user type;
+	try{
+		checkCommand();
+	} catch (const std::exception &e{
+		std::cerr << "input error: " << e.what();
 	}
-	else if (_prefixType == PREF_SERVER)
-	{
-		checkServerCommand();
-	}
+		
 
 }
