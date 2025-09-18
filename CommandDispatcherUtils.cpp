aff3d7c078be1b33643fec038ba511491741ebd3
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-09-18 12:43:07 +0200

    Update CommandDispatcherUtils.cpp
    changed .cpp in accordance with .hpp

--- CommandDispatcherUtils.cpp
+++ CommandDispatcherUtils.cpp
@@ -1,57 +1,57 @@
-#include "CommandHandler.hpp"
+#include "CommandDispatcher.hpp"
 
-int CommandHandler::handlePass(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchPass(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleNick(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchNick(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleUser(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchUser(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
 
-int CommandHandler::handleQuit(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchQuit(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleJoin(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchJoin(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleMode(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchMode(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleTopic(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchTopic(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0) << std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleInvite(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchInvite(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleKick(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchKick(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handlePrivmsg(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchPrivmsg(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< std::endl;
     return (RPL_WELCOME);
 }
 
-int CommandHandler::handleInfo(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatchInfo(std::vector <std::string> messageVec){
     std::cout << messageVec.at(0)<< "\n" <<std::endl;
     return (RPL_WELCOME);
 }
