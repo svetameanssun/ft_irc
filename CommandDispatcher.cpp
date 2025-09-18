c51890f9b5d01c533248b744f1c50c9969a51793
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-09-18 12:40:10 +0200

    Update CommandDispatcher.cpp
    Changed .cpp in conformity with .hpp

--- CommandDispatcher.cpp
+++ CommandDispatcher.cpp
@@ -1,29 +1,28 @@
-#include "CommandHandler.hpp"
+#include "CommandDispatcher.hpp"
 
 
-CommandHandler::CommandHandler() {
-
-    handlerMap["PASS"] = &CommandHandler::handlePass;
-    handlerMap["NICK"] = &CommandHandler::handleNick;
-	handlerMap["USER"] = &CommandHandler::handleUser;
-	handlerMap["QUIT"] = &CommandHandler::handleQuit;
-	handlerMap["JOIN"] = &CommandHandler::handleJoin;
-	handlerMap["MODE"] = &CommandHandler::handleMode;
-	handlerMap["TOPIC"] = &CommandHandler::handleTopic;
-	handlerMap["INVITE"] = &CommandHandler::handleInvite;
-	handlerMap["KICK"] = &CommandHandler::handleKick;
-	handlerMap["PRIVMSG"] = &CommandHandler::handlePrivmsg;
-	handlerMap["INFO"] = &CommandHandler::handleInfo;
+CommandDispatcher::CommandDispatcher() {
+    dispatcherMap["PASS"] = &CommandDispatcher::dispatchPass;
+    dispatcherMap["NICK"] = &CommandDispatcher::dispatchNick;
+	dispatcherMap["USER"] = &CommandDispatcher::dispatchUser;
+	dispatcherMap["QUIT"] = &CommandDispatcher::dispatchQuit;
+	dispatcherMap["JOIN"] = &CommandDispatcher::dispatchJoin;
+	dispatcherMap["MODE"] = &CommandDispatcher::dispatchMode;
+	dispatcherMap["TOPIC"] = &CommandDispatcher::dispatchTopic;
+	dispatcherMap["INVITE"] = &CommandDispatcher::dispatchInvite;
+	dispatcherMap["KICK"] = &CommandDispatcher::dispatchKick;
+	dispatcherMap["PRIVMSG"] = &CommandDispatcher::dispatchPrivmsg;
+	dispatcherMap["INFO"] = &CommandDispatcher::dispatchInfo;
 }
 
-const std::map<std::string, int (CommandHandler::*)(std::vector<std::string>)>& CommandHandler::getHandlerMap() const {
-    return handlerMap;
+const std::map<std::string, int (CommandDispatcher::*)(std::vector<std::string>)>& CommandDispatcher::getDispatcherMap() const {
+    return dispatcherMap;
 }
 
-int CommandHandler::handle(std::vector <std::string> messageVec){
+int CommandDispatcher::dispatch(std::vector <std::string> messageVec){
 	std::string cmd = messageVec.at(0);
-	if (handlerMap.find(cmd) != handlerMap.end()) {
-    	return (this->*handlerMap[cmd])(messageVec);
+	if (dispatcherMap.find(cmd) != dispatcherMap.end()) {
+    	return (this->*dispatcherMap[cmd])(messageVec);
 	}
 	else {
     	std::cerr << "Unknown command: " << cmd << std::endl;
