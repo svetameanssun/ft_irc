88fbc3651fadd7c304bbb634d7726c30bfcd586f
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-09-20 19:29:54 +0200

    Update ParcerResult.cpp

--- ParcerResult.cpp
+++ ParcerResult.cpp
@@ -3,7 +3,21 @@
 ParcerResult::ParcerResult(){
 }
 
+ void setChannelNamePassMap(std::map<std::string, std::string> joinResMap){
+   this->_channelNamePassMap = joinResMap;
+ }
 
+void setChannelnameVec(std::vector <std::string> joinResVec){
+  this->_channelNameVec = joinResVec;
+}
+
+cosnt std::map<std::string, std::string> getChannelNamePassMap(void) const{
+  return (_channelNamePassMap);
+}
+
+const std::vector <std::string> getChannelNameVec(void) const{
+  return (_channelNameVec);
+}
 
 ParcerResult::~ParcerResult(){
 }
