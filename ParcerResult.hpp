fc05c29b6c03051f22826a106fa9f1f3b30f64bb
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-09-20 19:26:22 +0200

    Update ParcerResult.hpp
    in develoment

--- ParcerResult.hpp
+++ ParcerResult.hpp
@@ -10,8 +10,10 @@ class ParcerResult{
   public:
     ParcerResult();
     ~ParcerResult();
+    void setChannelNamePassMap(std::map<std::string, std::string> joinResMap);
+    void setChannelnameVec(std::vector <std::string> joinResVec);
     cosnt std::map<std::string, std::string> getChannelNamePassMap(void) const;
-    const std::vector <std::string> getChannelNameVec;
+    const std::vector <std::string> getChannelNameVec(void) const;
   private:
     std::map<std::string, std::string> channelNamePassMap;
     std::vector <std::string> channelNameVec;
