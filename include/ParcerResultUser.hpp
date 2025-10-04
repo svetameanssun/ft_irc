fdc53ec0c9533ec2840e5f176f9278456210519b
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-10-04 20:40:22 +0200

    Create ParcerResultUser.hpp
    Created a header file for ParcerResultUser class. Realized that instead of using different member functions setUserParams, setPassParams, I can declare virtual  setParams in the parent class , in then redefine it in every derievd class!

---
+++ include/ParcerResultUser.hpp
@@ -0,0 +1,25 @@
+#ifndef PARCERRESULTUSER_HPP
+#define PARCERRESULTUSR_HPP
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include "AParcerResult.hpp"
+
+
+class ParcerResultUser : public AParcerResult {
+  public:
+    ParcerResultUser();
+    ParcerResultUser(const ParcerResultUser &other);
+    ParcerResultUser& operator=(const ParcerResultUser& other);
+    ~ParcerResultUser();
+    
+    void setUserParams(std::vector<std::string> userCommand);
+    const std::vector<std::string> getUserParams(void) const;
+    void printResult() const;
+  private:
+    std::vector <std::string> _userParamsVec;
+};
+
+#endif
