b17484bef73f820d3614eb6fc4fe41f38817919f
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-11-03 20:35:58 +0100

    Update ParcerResultKick.cpp
    in process

--- src/ParcerResultKick.cpp
+++ src/ParcerResultKick.cpp
@@ -72,7 +72,7 @@ bool ParcerResultKick::isValidChanName(std::string channelName) {
     size_t i = 0;
     if(channelName.empty()) {
         // NOT FORGET EVERYWHERE!
-        return false;
+        return (false);
     }
     if((channelName.at(0) != '#') && (channelName.at(0) != '&')) {
         std::cout << "THIS\n";
@@ -128,24 +128,102 @@ bool ParcerResultKick::isValidNick(std::string nickname) {
     return (1);
 }
 
-int ParcerResultKick::checkKickCommand(std::vector <std::string> messageVector){
+/*==========================================================*/
+/*----------------------------------------------------------*/
+/*                     IS_VALID_COMMAND                    */
+/*----------------------------------------------------------*/
+//It is important to use a reference here, 
+// because I will cut-off the trailing ending
+int  ParcerResultKick::checkKickComment (std::vector<std::string> &messageVector){
+	for(int i; i <messageVector.size(); i++){
+		if (messageVector[i].find(':') != std::string::npos)
+			break;
+	}
+	if (i + 1 == messageVec.size()){
+		this->_kickComment = "default";
+	}
+	else{
+		int save_i = i;
+		this->_kickComment.push_back('\0');
+		for (;i < messageVector.size(); i++){
+			this->_kickComment+=messageVector[i];
+		}
+		messageVector.resize(save_i + 1);
+	}
+	//I am not quite sure about this condition.
+	if (_kickComment.find('\r') != std::string::npos || _kickComment.find('\n') != std::string::npos){
+		return (ERR_UNKNOWNCOMMAND);
+	}
+	//if everything is OK returns 0;
+	return (0);
+}
+
+int  ParcerResultKick::fillKickParams(std::vector<std::string> messageVector){
+	std:vector <std::string> channels;
+	std:vector <std::string> users;
+	if (messageVector[1].find(',') != std::string::npos){
+		channels = stringToVec(messageVector[1], ',');
+	}
+	else{
+		channels[0] = messageVector[1];
+	}
+	if (messageVector[2].find(',') != std::string::npos){
+		users = stringToVec(messageVector[2], ',');
+	}
+	else{
+		users[0] = messageVector[2];
+	}
+	if (channels.size() != 1 && channels.size() != users.size()){
+		return (ERR_NEEDLESSPARAMS);
+	}
+	for (size_t i = 0; i < users.size(); i++){
+		(!isValidNick(users[i])){
+			return (ERR_UNKNOWNCOMMAND);
+		}
+		if (i < channels.size()){
+			if(!isValidChanName(channels[i])){
+				return (ERR_UNKNOWNCOMMAND);
+			}
+		}
+	}
+	this->_kickParamsMap = vectorsToMap(channels, users);
+	//if everything is OK returns 0;
+	return (0):
+}
+
+int ParcerResultKick::checkKickParams(std::vector <std::string> messageVector){
 	if (messageVector.size() <= 2){
 		return (ERR_NEEDMOREPARAMS);
 	int res;
-	if (res = setKickComment(messageVector) > 0); // if :trailing param exists, we will set it as _kickComment, if not, it will be default
-	// setKikComment also checks if the trailing comment is valid
+	if (res = checkKickComment(messageVector) > 0); // if :trailing param exists, we will set it as _kickComment, if not, it will be default
+	// setKickComment also checks if the trailing comment is valid
 	{
 		return res;
 	}
 	//after that the _kickParamsVec should already be set, without trailing params
-	if(messageVector.at(1).find(',') == std::string::npos){ // if there is  NO ',' in the string, there will be( one channel) AND (one OR many users) 
-		res = treatOneChannel(messageVector);
-	}
-	else {
-		res = treatManyChannels(messageVector);
-	}
+	res = checkChanAndNick(messageVector);
 	return (res);
 }
+/*==========================================================*/
+
+/*----------------------------------------------------------*/
+/*                   SPLIT_AND_UNITE                        */
+/*----------------------------------------------------------*/
+const std::vector<std::string> ParcerResultKick::stringToVec(std::string str, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string item;
+
+    while(getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+    return result;
+}
+
+std::map<int, std::vector<std::string>> ParcerResultKick::vectorsToMap(std::vector<std::string> channels, std::vector<std::string>users){
+	for (size_t i = 0; )
+}
+
 /*----------------------------------------------------------*/
 /*                      PRINT_RESULT                        */
 /*----------------------------------------------------------*/
