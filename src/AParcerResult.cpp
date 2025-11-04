05538899cd83d46254790b63fb3f1bf9cc9ea98a
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-11-04 19:09:00 +0100

    Add IS_VALID_CHANNEL_NAME
    Added destructor and validation methods for channel names.

--- src/AParcerResult.cpp
+++ src/AParcerResult.cpp
@@ -1,6 +1,9 @@
 #include "AParcerResult.hpp"
+/*----------------------------------------------------------*/
+/*                      CANONICAL PART                      */
+/*----------------------------------------------------------*/
 
-AParcerResult::AParcerResult(){};
+AParcerResult::AParcerResult(){}
 
 AParcerResult::AParcerResult(std::string command){
     this->_command = command;
@@ -21,3 +24,51 @@ AParcerResult& AParcerResult::operator=(const AParcerResult& other){
 std::string AParcerResult::getCommand()const{
     return (_command);
 }
+
+AParcerResult::~ AParcerResult() {} // "required if not using Microsoft extension" taken from https://learn.microsoft.com/en-us/cpp/cpp/abstract-classes-cpp?view=msvc-170
+/*==========================================================*/
+/*----------------------------------------------------------*/
+/*                   IS_VALID_CHANNEL_NAME                  */
+/*----------------------------------------------------------*/
+bool AParcerResult::isValidChanNameChar(int c) {
+    if(c == '\0')
+        return (false);
+    else if(c == '\a')
+        return (false);
+    else if(c == '\r')
+        return (false);
+    else if(c == '\n')
+        return (false);
+    else if(c == ' ')
+        return (false);
+    else if(c == ',')
+        return (false);
+    else if(c == ':')
+        return (false);
+    else {
+        return (true);
+    }
+}
+
+bool AParcerResult::isValidChanName(std::string channelName) {
+    size_t i = 0;
+    if(channelName.empty()) {
+        // NOT FORGET EVERYWHERE!
+        return (false);
+    }
+    if((channelName.at(0) != '#') && (channelName.at(0) != '&')) {
+        std::cout << "THIS\n";
+        return (false);
+    }
+    if((channelName.length() > 200) || (channelName.length() == 1)) {
+        return (false);
+    }
+    i++;
+    for(; i < channelName.length(); ++i) {
+        if(!isValidChanNameChar(channelName.at(i))) {
+            return (false);
+        }
+    }
+    return (true);
+}
+/*==========================================================*/
