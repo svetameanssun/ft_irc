deccdf3dcfc8ed845a2845c7f3053b760773b4bb
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-11-04 20:17:55 +0100

    Refactor ParcerResultMode to ParcerResultPrivmsg
    Updated ParcerResultMode to ParcerResultPrivmsg with new command handling for private messages.

--- src/ParcerResultPrivmsg.cpp
+++ src/ParcerResultPrivmsg.cpp
@@ -1,25 +1,75 @@
-#include "ParcerResultMode.hpp"
+#include "ParcerResultPrivmsg.hpp"
 /*----------------------------------------------------------*/
 /*                      CANONICAL PART                      */
 /*----------------------------------------------------------*/
-ParcerResultMode::ParcerResultMode()
+ParcerResultPrivmsg::ParcerResultPrivmsg()
     : AParcerResult(){
-        _command = "MODE";
+        _command = "PRIVMSG";
 }
 
-ParcerResultMode::ParcerResultMode(const ParcerResultMode &other){
+ParcerResultPrivmsg::ParcerResultPrivmsg(const ParcerResultPrivmsg &other){
     this->_command = other._command;
-    this->_modeParamsVec = other._modeParamsVec;
-    this->_modeParamsMap = other._modeParamsMap;
+    //this->_privmsgParamsVec = other._privmsgParamsVec;
+    //this->_privmsgParamsMap = other._privmsgParamsMap;
 }
 
-ParcerResultMode& ParcerResultMode::operator=(const ParcerResultMode& other){
+ParcerResultPrivmsg& ParcerResultPrivmsg::operator=(const ParcerResultPrivmsg& other){
     if(this != &other) {
-        this->_modeParamsVec = other._modeParamsVec;
-        this->_modeParamsMap = other._modeParamsMap;
+        //this->_privmsgParamsVec = other._privmsgParamsVec;
+        //this->_privmsgParamsMap = other._privmsgParamsMap;
         this->_command = other._command;
     }
     return (*this);
 }
 
-ParcerResultMode::~ParcerResultMode(){}
+ParcerResultPrivmsg::~ParcerResultPrivmsg(){}
+
+/*
+1 Private messages
+ Command: PRIVMSG
+ Parameters: <msgtarget> <text to be sent>
+ PRIVMSG is used to send private messages between users, as well as to
+ send messages to channels. <msgtarget> is usually the nickname of
+ the recipient of the message, or a channel name.
+ The <msgtarget> parameter may also be a host mask (#<mask>) or server
+ mask ($<mask>). In both cases the server will only send the PRIVMSG
+ to those who have a server or host matching the mask. The mask MUST
+ have at least 1 (one) "." in it and no wildcards following the last
+ ".". This requirement exists to prevent people sending messages to
+ "#*" or "$*", which would broadcast to all users. Wildcards are the
+ ’*’ and ’?’ characters. This extension to the PRIVMSG command is
+ only available to operators.
+
+ Examples:
+ :Angel!wings@irc.org PRIVMSG Wiz :Are you receiving this message ?
+                                                         Message from Angel to Wiz.
+ PRIVMSG Angel :yes I’m receiving it !
+                                                         Command to send a message to Angel.
+ PRIVMSG jto@tolsun.oulu.fi :Hello !
+                                                         Command to send a message to a user
+                                                         on server tolsun.oulu.fi with
+                                                         username of "jto".
+ PRIVMSG kalt%millennium.stealth.net@irc.stealth.net :Are you a frog?
+                                                         Message to a user on server
+                                                         irc.stealth.net with username of
+                                                        "kalt", and connected from the host
+                                                         millennium.stealth.net.
+ PRIVMSG kalt%millennium.stealth.net :Do you like cheese?
+                                                        Message to a user on the local
+                                                        server with username of "kalt", and
+                                                        connected from the host
+                                                        millennium.stealth.net.
+ PRIVMSG Wiz!jto@tolsun.oulu.fi :Hello !
+                                                         Message to the user with nickname
+                                                         Wiz who is connected from the host
+                                                         tolsun.oulu.fi and has the username
+                                                         "jto".
+ PRIVMSG $*.fi :Server tolsun.oulu.fi rebooting.
+                                                         Message to everyone on a server
+                                                         which has a name matching *.fi.
+ 
+ PRIVMSG #*.edu :NSFNet is undergoing work, expect interruptions
+                                                         Message to all users who come from
+                                                         a host which has a nam
+
+*/
