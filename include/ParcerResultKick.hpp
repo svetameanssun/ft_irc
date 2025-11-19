0a0ce17bb22b169220f6c50a645d8a6ae579af3c
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-11-19 19:52:05 +0100

    Update ParcerResultKick.hpp
    Added two attributes: _kickUsersVec and _kickChannelsVec

--- include/ParcerResultKick.hpp
+++ include/ParcerResultKick.hpp
@@ -19,7 +19,9 @@ public:
     /*================= SETTERS / GETTERS ===============*/
     void setParams(std::vector<std::string> kickCommand);
     const std::vector<std::string> getKickParams(void) const;
-
+    const std::vector<std::string> getKickChannelsVec(void) const;
+    const std::vector<std::string> getKickUsersVec(void) const;
+    
     /*================= VALIDATION ======================*/
     //bool isValidChanNameChar(int c);
     //bool isValidChanName(std::string channelName);
@@ -43,7 +45,9 @@ public:
     void printResult(void) const;
 private:
     std::vector<std::string> _kickParamsVec;   // Stores raw parameters (after "KICK")
-    std::map<int, std::vector<std::string> > _kickParamsMap;  // Maps index → [channel, user]
+    std::map<int, std::vector<std::string> > _kickParamsMap;  // Maps index → [index] = {channel, user}
+    std::vector <std::string> _kickChannelsVec;// channels in order (where the user's being kicked out)
+    std::vector <std::string> _kickUsersVec; // users being kicked out
     std::string _kickComment;                  // Optional trailing comment
 };
 
