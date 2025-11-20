d24b61347a8c29ad4ac557dd34a6ca2a1739b134
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-11-20 20:18:06 +0100

    Delete isValidNick and isSpecialChar functions
    Removed validation functions for nicknames.

--- src/ParcerResultNick.cpp
+++ src/ParcerResultNick.cpp
@@ -36,46 +36,6 @@ void ParcerResultNick::setParams(std::vector<std::string> nickCommand) {
 const std::string ParcerResultNick::getNickname(void)const {
     return (this->_nickname);
 }
-
-/*==========================================================*/
-
-/*----------------------------------------------------------*/
-/*                   IS_VALID / IS_SPECIAL                  */
-/*----------------------------------------------------------*/
-
-bool ParcerResultNick::isSpecialChar(int c) {
-    std::string specialChars = "[]\\`^{}";
-    if(specialChars.find(c) == std::string::npos) {
-        std::cout << "char: " << c << "\n";
-        return (0);
-    }
-    return (1);
-}
-
-bool ParcerResultNick::isValidNick(std::vector<std::string> nickCommand) {
-    
-    std::string nickname = nickCommand.at(1);
-    std::cout << nickname << "\n";
-    if(nickname.empty()) {
-        return (0);
-    }
-    if(nickname.at(0) == '-' || isdigit(nickname.at(0))) {
-        return (0);
-    }
-    if(!isalpha(nickname.at(0)) && !isSpecialChar(nickname.at(0))) {
-        return (0);
-    }
-    if(nickname.length() > 9) {
-        return (0);
-    }
-    for(long unsigned int i = 1; i < nickname.length(); ++i) {
-        if(!isdigit(nickname.at(i)) && !isalpha(nickname.at(i)) &&
-                !isSpecialChar(nickname.at(i)) && nickname.at(i) != '-') {
-            return (0);
-        }
-    }
-    return (1);
-}
 /*==========================================================*/
 
 /*----------------------------------------------------------*/
