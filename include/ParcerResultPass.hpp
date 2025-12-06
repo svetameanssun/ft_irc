c2aaf80388c42518c304d2bba3c7f092a1cb79ec
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-12-06 15:53:15 +0100

    Update ParcerResultPass.hpp

--- include/ParcerResultPass.hpp
+++ include/ParcerResultPass.hpp
@@ -11,14 +11,19 @@
 
 class ParcerResultPass : public AParcerResult {
   public:
+    /*================= CANONICAL FORM =================*/
     ParcerResultPass();
     ParcerResultPass(const ParcerResultPass &other);
     ParcerResultPass& operator=(const ParcerResultPass& other);
     ~ParcerResultPass();
-  
+
+    /*================= SETTERS / GETTERS ===============*/
     void setParams(std::vector<std::string> passCommand);
     const std::vector<std::string> getPassParams(void) const;
+
+     /*================= DEBUG / OUTPUT ==================*/
     void printResult() const;
+
   private:
     std::vector <std::string> _passParamsVec; // to make it unified,
                                           // I pass the password as a vec, but it is actually a string
