3ce5d235909872c8cd0f59ae90a55a454208ad93
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2025-12-06 16:12:37 +0100

    Update ParcerResultTopic.hpp

--- include/ParcerResultTopic.hpp
+++ include/ParcerResultTopic.hpp
@@ -10,16 +10,23 @@
 
 class ParcerResultTopic : public AParcerResult {
   public:
+  /*================= CANONICAL FORM =================*/
     ParcerResultTopic();
     ParcerResultTopic(const ParcerResultTopic &other);
     ParcerResultTopic& operator=(const ParcerResultTopic& other);
     ~ParcerResultTopic();
     
+  /*================= SETTERS / GETTERS ===============*/
     void setParams(std::vector<std::string> topicCommand);
     const std::vector<std::string> getTopicParams(void) const;
-    void printResult() const;
-    //bool isAllowedChar(char usernameChar);
+    const std::string getTopicMessage(void) const;
+
+    /*================= COMMAND CHECKING ================*/
     //int checkTopicParams(std::vector<std::string> messageVec);
+    //bool isAllowedChar(char usernameChar);
+  
+    /*================= DEBUG / OUTPUT ==================*/
+    void printResult() const;
   private:
     std::vector <std::string> _topicParamsVec;
     std::string _topicMessage;
