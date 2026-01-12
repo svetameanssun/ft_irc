e507e659506b9601d9c412bdf0d5a79ad8963ac9
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-01-12 13:24:21 +0100

    Fix class name typo from CommandParcer to CommandParser

--- include/CommandParser.hpp
+++ include/CommandParser.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMANDPARCER_HPP
-#define COMMANDPARCER_HPP
+#ifndef COMMANDPARSER_HPP
+#define COMMANDPARSER_HPP
 
 #include <iostream>
 #include <sstream>
@@ -15,11 +15,11 @@
 #include "CommandDispatcher.hpp"
 
 
-class CommandParcer {
+class CommandParser {
   public:
-    CommandParcer(const std::string &message): _message(message) {};
-    CommandParcer(const CommandParcer&other);
-    CommandParcer&operator=(const CommandParcer&other);
+    CommandParser(const std::string &message): _message(message) {};
+    CommandParcer(const CommandParser&other);
+    CommandParser&operator=(const CommandParser&other);
     ~CommandParcer();
 
     bool splitMessage(void);
@@ -35,7 +35,7 @@ class CommandParcer {
     CommandDispatcher _cmdDisp;
 };
 /**
- * @brief parces and analizes the command and its params.
+ * @brief parses and analizes the command and its params.
  *
  * This function initiates parcing, analizes the command and parameter
  * structure, and passes the data to the server.
@@ -44,9 +44,5 @@ class CommandParcer {
  *		LATER Parcer &parcer, to init Parcer class outside
  * @returns RPL/0 on  success or l ERR (from Replies.hpp)
  */
-
-//TEMPORAL, later this function (or its content) will fit into some part of the server's code
-//int launchParcing(std::string messageStr);
-
 
 #endif
