90aa69ac96f56fcd8cff72aaaa9ef15f8228f921
Ruben Egea Gonzalez <rubenegea@MacBook-Air-de-Ruben.local>
2026-01-22 16:58:41 +0100

    otro merge

--- include/ParserResultUser.hpp
+++ include/ParserResultUser.hpp
@@ -19,7 +19,6 @@ class ParserResultUser : public AParserResult {
     /*================= SETTERS / GETTERS ===============*/
     void setParams(std::vector<std::string> userCommand);
     void setRealname(std::string name);
-    void setRealname(std::string name);
     const std::vector<std::string> getUserParams(void) const;
     const std::string getRealname(void) const;
     const std::string getUsername(void) const;
