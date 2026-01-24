ff91a740c300f7f8520ab5d2fab9fba4e4ee831c
svetameanssun <114097908+svetameanssun@users.noreply.github.com>
2026-01-24 20:42:50 +0100

    Refactor _privmsgMessage to remove leading/trailing spaces
    Trim leading and trailing whitespace from _privmsgMessage.

--- src/ParserResultPrivmsg.cpp
+++ src/ParserResultPrivmsg.cpp
@@ -95,11 +95,14 @@ int ParserResultPrivmsg::checkPrivmsgParams(std::vector <std::string> messageVec
         for (size_t i = 2; i < messageVector.size(); ++i){
             this->_privmsgMessage += messageVector[i];
             this->_privmsgMessage += " ";
-
         }
-        this->_privmsgMessage.erase(0, 1); // It removes the 1st char
+        if (this->_privmsgMessage[0] == ':'){
+            this->_privmsgMessage.erase(0, 1); // It removes the 1st char
                                             // (which in this case is ':')
-        this->_privmsgMessage.erase(_privmsgMessage.size()-1, 1);                              
+        }
+        if (this->_privmsgMessage[_privmsgMessage.length()-1] == ' '){
+            this->_privmsgMessage.erase(_privmsgMessage.length()-1, 1);
+        }
     }
     int res = checkPrivmsgTarget(messageVector[1]);
     if (res > 0){
